604b7f9deec08f9bccf9403ece65321d9cbf807a
Shruthika <100517924+Shruthika16@users.noreply.github.com>
2023-08-31 15:06:32 +0530

    Add files via upload

---
+++ daytwo_class_ex/src/main/java/com/example/demo/DaytwoClassExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaytwoClassExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaytwoClassExApplication.class, args);
+	}
+
+}
