3c889ee4e91aad26a5d6a61a243ccb7147ea27cb
Shruthika <100517924+Shruthika16@users.noreply.github.com>
2023-08-31 15:09:26 +0530

    Add files via upload

---
+++ dayfour_class_ex/src/main/java/com/example/demo/DayfourClassExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayfourClassExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayfourClassExApplication.class, args);
+	}
+
+}
---
+++ dayfour_class_ex/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+
+import com.example.demo.service.StudentService;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentService sser;
+	
+	@PostMapping("addstudent")
+	public Student add(@RequestBody Student ss)
+	{
+		return sser.saveinfo(ss);
+	}
+	
+	@GetMapping("showdetails")
+	public List<Student> show(){
+		return sser.showinfo();
+	}
+
+}
